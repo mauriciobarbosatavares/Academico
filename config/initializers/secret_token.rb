24efe18acbd203d8313498a3a18535e17106bd62
Lucas Castro <lucasmcastro@gmail.com>
2012-03-31 16:40:53 -0300

    first commit

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Academico::Application.config.secret_token = '71b8cbedbc78b140dc16eb05b2fcf1b6a64889bf1db910a1bb2f0384a1439428e9827d2db744999a38845dc7b90a3d9d3c57652554830197aed3a5ca8af45a78'
